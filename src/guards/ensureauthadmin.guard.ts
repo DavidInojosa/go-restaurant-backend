c9000bd06c0d3799ba8e9f1c85963465a5524d29
DavidInojosa <49165221+DavidInojosa@users.noreply.github.com>
2022-06-09 16:56:35 -0300

    (feat) Guard - Ensure Admin Auth

---
+++ src/guards/ensureauthadmin.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { verify } from 'jsonwebtoken';
+import { Observable } from 'rxjs';
+
+interface IPayload {
+  sub: string;
+}
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers;
+
+    if (!authHeader) {
+      return false;
+    }
+
+    const token = authHeader.Authorization;
+
+    try {
+      verify(token, '418a47914d81db9fe17f01ab1aed1c71') as IPayload;
+      // md5 hash
+
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+}
